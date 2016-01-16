bbb8db777cbbcc5222e250388d4788d38b9b62f7
Isma <ismaharo>
2016-01-16 14:38:43 -0600

    Probando app

--- js/almacen.js
+++ js/almacen.js
@@ -4,13 +4,15 @@ var almacen = {
 	numPersonas    : null,
 	numHabitaciones: null,
 	numDias        : null,
-	
+
 	guardarReserva: function(tipoH, numP, numH, numD){
+alert("guardando Reserva");
 		almacen.db = window.openDatabase("hotelApp", "1.0", "Hotel App", 2000);
 		almacen.tipoHabitacion  = tipoH;
 		almacen.numPersonas     = numP;
 		almacen.numHabitaciones = numH;
 		almacen.numDias         = numD;
+alert("Iniciando transaccion");
 		almacen.db.transaction(almacen.tablaReserva, almacen.error, almacen.confirmarReservaGuardada);
 	},
 
@@ -19,19 +21,21 @@ var almacen = {
 	},
 
 	tablaReserva: function(tx){
+alert("CREANDO TABLA");
 		// CREAR TABLA SI TODAVIA NO EXISTE
 		tx.executeSql('CREATE TABLE IF NOT EXISTS reservas (id INTEGER PRIMARY KEY, 
 			                                                tipoh, 
 			                                                nump, 
 			                                                numh, 
 			                                                numd)');
-
+alert("INSERTANDO DATOS");
 		// INSERTAR DATOS EN LA TABLA DE "RESERVAS"
 		tx.executeSql('INSERT INTO reservas (tipoh, nump, numh, numd)
 			           VALUES ("'+almacen.tipoHabitacion+'", 
 			           	       "'+almacen.numPersonas+'", 
 			           	       "'+almacen.numHabitaciones+'",
 			           	       "'+almacen.numDias+'")');
+alert("DATOS INSERTADOS");		
 	},
 
 	confirmarReservaGuardada: function(){
