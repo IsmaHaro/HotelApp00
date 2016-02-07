34a04cf68010944dcbf3529b31b4e5539ebaee25
Isma <ismaharo>
2016-02-06 21:21:12 -0600

    Asd

--- js/API/geolocation.js
+++ js/API/geolocation.js
@@ -7,7 +7,7 @@ var geolocation = {
 	},
 
 	exito: function(position){
-alert("Position: "+position.coords.latitude+" "+position.coords.latitude);
+alert("Position: "+position.coords.latitude+" "+position.coords.longitude);
 		geolocation.latitud = position.coords.latitude;
 		geolocation.longitud = position.coords.longitude;
 	},
